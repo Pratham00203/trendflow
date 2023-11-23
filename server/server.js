18d9747d17cbcb5e48bab34fef18019d74cd724a
Pratham Shelar <shelarpratham80@gmail.com>
2023-11-23 19:37:06 +0530

    Some changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const fileUpload = require("express-fileupload");
 const connectDB = require("./db/db");
 const PORT = process.env.PORT || 5000;
 
-app.use("/public", express.static("public"));
+app.use("/uploads", express.static("public/uploads"));
 app.use(cors());
 app.use(
   fileUpload({
